e2db84701f13612323dd2d86ca37087e940ef3b8
Ravi <143279437+RaviAdvt@users.noreply.github.com>
2024-03-05 01:06:36 +0530

    Codes

---
+++ java+sel+learning.java
@@ -0,0 +1,25 @@
+package testmain;
+import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.firefox.FirefoxDriver;
+public class testmain {
+
+public static void main(String[] args) {
+// TODO Auto-generated method stub
+//System.out.println(hello);
+//invoking browser
+//class classname -> new classmethod
+//chrome - chromedriver -> methods(get, close etc)
+
+//driver = webdriver methods(selenium -> interface webdriver) + class method(browser driver class)
+
+//ChromeDriver driver = new ChromeDriver();
+//FirefoxDriver driver = new FirefoxDriver();
+//System.setProperty(chrome.driver,/home/ravi/.local/bin/chromedriver);
+System.setProperty(webdriver.chrome.driver, /home/ravi/Downloads/chromedriver-linux64/chromedriver);
+WebDriver driver = new ChromeDriver();
+
+}
+
+}
+
